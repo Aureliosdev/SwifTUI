e72238024639abb2c3ab307fe1370692bc9f874d
Abylay <aurelioleclarke@gmail.com>
2022-02-21 12:56:04 +0600

    Just a fun news

---
+++ Hacker News/Hacker News/PostData.swift
@@ -0,0 +1,21 @@
+//
+//  PostData.swift
+//  Hacker News
+//
+//  Created by Aurelio Le Clarke on 21.02.2022.
+//
+
+import Foundation
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
